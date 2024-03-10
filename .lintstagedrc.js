c58d2592fb8dfc23d2b830007270b46faa44b2c3
Dailton Bastos <dailtonbastos@gmail.com>
2024-03-10 18:05:06 -0300

    fix(lint): configure lint-staged for eslint warning

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,17 +1,23 @@
 const path = require('path')
 
 const buildEslintCommand = (filenames) => {
-	if (filenames.length > 10) return 'next lint --fix .'
+	const lintOptions =
+		'next lint --fix --max-warnings=0 --config .eslintrc.config.json'
+
+	if (filenames.length > 10) return `${lintOptions} .`
 
 	const cwd = process.cwd()
 	const relativePaths = filenames.map((file) => path.relative(cwd, file))
 
-	return `next lint --fix --file ${relativePaths.join(' --file ')}`
+	return `${lintOptions} --file ${relativePaths.join(' --file ')}`
 }
 
 const buildPrettierCommand = 'prettier --write --ignore-unknown'
 
 module.exports = {
-	'*.{js,ts,tsx}': [buildEslintCommand, buildPrettierCommand],
+	'!(.lintstagedrc.js)*.{js,ts,tsx}': [
+		buildEslintCommand,
+		buildPrettierCommand,
+	],
 	'*.{md,json}': [buildPrettierCommand],
 }
